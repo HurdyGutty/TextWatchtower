6d49720117949701598984fb1d23624ff79c651a
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-08-01 19:19:07 +0700

    Finish: reloadPoint package

---
+++ pkg/reloadPoint/reloadPoint.go
@@ -0,0 +1,28 @@
+package reloadPoint
+
+import (
+	"fmt"
+
+	"github.com/go-vgo/robotgo"
+	hook "github.com/robotn/gohook"
+)
+
+type MousePoint struct {
+	X int
+	Y int
+}
+
+func ReloadPoint() *MousePoint {
+	point := new(MousePoint)
+	s := hook.Start()
+	fmt.Println("--- Please press Esc to stop hook ---")
+	hook.Register(hook.KeyDown, []string{"esc"}, func(e hook.Event) {
+		fmt.Println("Escaped")
+		hook.End()
+	})
+	if hook.AddMouse("left") {
+		point.X, point.Y = robotgo.Location()
+	}
+	<-hook.Process(s)
+	return point
+}
