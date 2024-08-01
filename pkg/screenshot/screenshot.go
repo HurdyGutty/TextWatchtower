5a6f538145e855bc32ca4abefb8209773a5a4e62
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-08-01 19:02:46 +0700

    Refactor: change screenshot variable names to match robotgo CaptureScreen variables

--- pkg/screenshot/screenshot.go
+++ pkg/screenshot/screenshot.go
@@ -4,8 +4,8 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-func TakeScreenShot(x1, x2, y1, y2 int, imgName string) {
-	bit := robotgo.CaptureScreen(x1, y1, x2, y2)
+func TakeScreenShot(x1, y1, w, h int, imgName string) {
+	bit := robotgo.CaptureScreen(x1, y1, w, h)
 	defer robotgo.FreeBitmap(bit)
 
 	img := robotgo.ToImage(bit)
