f72bb89bf2a7b24e641ad41047a4accf43c4675e
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-08-13 13:50:17 +0700

    Add: CreateImagePath

--- pkg/screenshot/screenshot.go
+++ pkg/screenshot/screenshot.go
@@ -4,10 +4,14 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-func TakeScreenShot(x1, y1, w, h int, imgName string) {
+func CreateImagePath(imgName string) string {
+	return "images/" + imgName + ".png"
+}
+
+func TakeScreenShot(x1, y1, w, h int, imgPath string) {
 	bit := robotgo.CaptureScreen(x1, y1, w, h)
 	defer robotgo.FreeBitmap(bit)
 
 	img := robotgo.ToImage(bit)
-	robotgo.Save(img, "images/"+imgName+".png")
+	robotgo.Save(img, imgPath)
 }
