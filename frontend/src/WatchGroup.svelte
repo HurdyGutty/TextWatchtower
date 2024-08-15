7ddedfbab7e290a3bdf0108a6d8ae1e403bdd6cf
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-08-15 16:49:34 +0700

    refactor: use the new store and add GroupMenu

--- frontend/src/WatchGroup.svelte
+++ frontend/src/WatchGroup.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { StartOverwatch, StopOverwatch, NewCaptureGroup, DeleteGroup } from "../wailsjs/go/main/App.js";
-    import { InstructionAlert, InstructionError, InstructionInfo } from "../wailsjs/go/instruct/InstructionBoard.js";
-    type Group = {
-        id : number
-    }
-    const groups: Group[] = [];
+    import { InstructionAlert, InstructionError, InstructionInfo } from "../wailsjs/go/instruct/InstructionBoard";
+    import { type Group, groupsMap } from "./stores"
+    import GroupMenu from "./GroupMenu.svelte";
     let start = false;
-    const numberQueue = [1,2,3,4,5,6]
+    let numberQueue = [1,2,3,4,5,6]
+    let groups: Map<number, Group>;
+    groupsMap.subscribe((value) => {
+        groups = value;
+    })
     function startOverWatch(e: Event, id: number) {
         e.preventDefault();
         StartOverwatch(id);
@@ -25,16 +27,21 @@
             return;
         }
         let id = numberQueue.shift()
+        numberQueue = numberQueue
         NewCaptureGroup(id);
+        groups = groups.set(id, {id})
     }
 
     async function deleteGroup(e: Event, id: number) {
         e.preventDefault();
-        if (groups.length === 0) {
+        if (groups.size === 0) {
             InstructionError("There is no more group to delete")
             return;
         }
         numberQueue.push(id)
+        numberQueue = numberQueue
+        groups.delete(id)
+        groups = groups
         let deletedId = await DeleteGroup(id, InstructionError)
         InstructionInfo(`Delete Group ${deletedId}`)
     }
@@ -42,9 +49,9 @@
 </script>
 
 <div id="group-container">
-    {#each groups as group}
+    {#each Array.from(groups.values()) as group}
         <div class={group.id.toString()}>
-            <p>Group {group.id}</p>
+            <span>Group {group.id}</span>
             {#if !start}
             <button  on:click={(e) => startOverWatch(e, group.id)}>
                 <img src="assets/images/play-button.svg" alt="Start"/>
@@ -57,9 +64,10 @@
             <button on:click={(e) => deleteGroup(e, group.id)}>
                 <img src="assets/images/delete-button.svg" alt ="Delete" />
             </button>
+            <GroupMenu group={group}></GroupMenu>
         </div>
     {/each}
-    {#if groups.length < 6}
+    {#if groups.size < 6}
         <button class="0" on:click={(e) => addNewGroup(e)}>+</button>
     {/if}
 </div>
