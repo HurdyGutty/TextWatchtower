96521f1db7ef40c478649ea262fdf37eb6a7d94e
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-08-16 01:02:03 +0700

    add: new canvas to watch windows

---
+++ frontend/src/Canvas.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { type Group } from "./stores";
+
+    let canvas: HTMLCanvasElement
+    let c = canvas.getContext("2d")
+    const elementRect = canvas.getBoundingClientRect()
+
+    export function showWatchBox (group: Group) {
+        if ("watchBox" in group){
+            c.strokeRect(group.watchBox.x - elementRect.left, group.watchBox.y - elementRect.top, group.watchBox.w, group.watchBox.h)
+        }
+    }
+
+    export function showReloadPoint (group: Group) {
+        const radius = 5;
+        if ("reload" in group) {
+            c.beginPath();
+            c.arc(group.reload.X, group.reload.Y, radius, 0, 2 * Math.PI, false);
+            c.fillStyle = "green";
+            c.fill();
+        }
+        
+    }
+
+    export function clearCanvas() {
+        c.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+
+</script>
+
+<canvas bind:this={canvas}></canvas>
+
+<style>
+    canvas {
+        border: 1px solid black;
+
+    }
+</style>
