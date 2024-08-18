c66e38bfabf4fc875aba8db95e4d324c53e77d2f
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-08-18 12:28:19 +0700

    add: title component

---
+++ frontend/src/Title.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+</script>
+
+<div id="title">
+    <img src="assets/image/watchtower.png" alt="Program icon"/>
+    <h1>Text Watchtower</h1>
+</div>
+
+<style>
+    h1 {
+        color: black;
+    }
+
+    img {
+        max-height: 10px;
+    }
+
+    #title {
+        height: 40px;
+        display: flex;
+        text-align: center;
+
+    }
+</style>
