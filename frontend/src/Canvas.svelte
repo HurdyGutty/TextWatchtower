521531f0a4d16bce302828ad3f16164f5ced1980
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-03 18:28:38 +0700

    refactor: Canvas function and CSS

--- frontend/src/Canvas.svelte
+++ frontend/src/Canvas.svelte
@@ -1,39 +1,42 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import { type Group } from "./stores";
 
     let canvas: HTMLCanvasElement
-    let c = canvas.getContext("2d")
-    const elementRect = canvas.getBoundingClientRect()
+    let c: CanvasRenderingContext2D;
+    let elementRect: DOMRect
+    const radius = 5
+    onMount(() => {
+        c = canvas.getContext('2d')
+        elementRect = canvas.getBoundingClientRect()
+    })
 
-    export function showWatchBox (group: Group) {
+    function clearCanvas() {
+        c.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+    export function drawCanvas(group: Group) {
+        clearCanvas()
         if ("watchBox" in group){
             c.strokeRect(group.watchBox.x - elementRect.left, group.watchBox.y - elementRect.top, group.watchBox.w, group.watchBox.h)
         }
-    }
-
-    export function showReloadPoint (group: Group) {
-        const radius = 5;
         if ("reload" in group) {
             c.beginPath();
             c.arc(group.reload.X, group.reload.Y, radius, 0, 2 * Math.PI, false);
             c.fillStyle = "green";
             c.fill();
         }
-        
     }
-
-    export function clearCanvas() {
-        c.clearRect(0, 0, canvas.width, canvas.height)
-    }
-
 
 </script>
+
 
 <canvas bind:this={canvas}></canvas>
 
 <style>
     canvas {
         border: 1px solid black;
-
+        margin: 3rem;
+        width: 80vw;
     }
 </style>
