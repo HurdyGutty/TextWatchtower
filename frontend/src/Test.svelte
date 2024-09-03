65e2cf2aed2c6e6b71a357926f95bb2dfe17f80c
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-03 18:25:47 +0700

    add: Test component

---
+++ frontend/src/Test.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import GroupMenu from "./GroupMenu.svelte";
+    import { groupsMap, type Group } from "./stores"
+    export let drawCanvasFn: (group: Group) => void
+
+    let groups: Map<number, Group>
+
+    groupsMap.subscribe((value) => {
+        groups = value
+    })
+
+    let group = groups.get(1)
+    
+</script>
+
+{#if typeof group === "undefined"}
+<div>1</div>
+{:else}
+<div on:click={() => drawCanvasFn(group)}
+    on:keyup={() => drawCanvasFn(group)}
+    role="button"
+    tabindex="0">{group.id}</div>
+<GroupMenu group={group} drawCanvas={drawCanvasFn}/>
+{/if}
