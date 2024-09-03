ba97b3dcb73a3535f689c923202f760e6dc0ac94
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-03 18:31:42 +0700

    refactor: App component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,80 +1,35 @@
-<script>
-  import { GetRandomImageUrl } from "../wailsjs/go/main/App.js";
-  import { GetBreedList } from "../wailsjs/go/main/App.js";
-  import { GetImageUrlsByBreed } from "../wailsjs/go/main/App.js";
+<script lang="ts">
+  import Canvas from "./Canvas.svelte"
+  import InstructionBoard from "./InstructionBoard.svelte"
+  import GroupContainer from "./GroupContainer.svelte"
+  import Title from "./Title.svelte"
+  // import Test from "./Test.svelte";
+  import { type Group } from "./stores";
+  import { onMount } from "svelte";
 
-  let randomImageUrl = "";
-  let breeds = [];
-  let photos = [];
-  let selectedBreed;
-  let showRandomPhoto = false;
-  let showBreedPhotos = false;
+  let canvas: Canvas
+  let drawCanvasFn: (group: Group) => void
 
-  function init() {
-    getBreedList();
-  }
+  onMount(() => {
+    drawCanvasFn = canvas.drawCanvas
+  })
 
-  init();
-
-  function getRandomImageUrl() {
-    showRandomPhoto = false;
-    showBreedPhotos = false;
-    GetRandomImageUrl().then((result) => (randomImageUrl = result));
-    showRandomPhoto = true;
-  }
-
-  function getBreedList() {
-    GetBreedList().then((result) => (breeds = result));
-  }
-
-  function getImageUrlsByBreed() {
-    init();
-    showRandomPhoto = false;
-    showBreedPhotos = false;
-    GetImageUrlsByBreed(selectedBreed).then((result) => (photos = result));
-    showBreedPhotos = true;
-  }
 </script>
 
-<h3>Dogs API</h3>
 <div>
-  <button class="btn" on:click={getRandomImageUrl}>
-    Fetch a dog randomly
-  </button>
-  Click on down arrow to select a breed
-  <select bind:value={selectedBreed}>
-    {#each breeds as breed}
-      <option value={breed}>
-        {breed}
-      </option>
-    {/each}
-  </select>
-  <button class="btn" on:click={getImageUrlsByBreed}>
-    Fetch by this breed
-  </button>
+  <Title/>
+  <InstructionBoard/>
+  <GroupContainer drawFn={drawCanvasFn}/>
+  <Canvas bind:this={canvas}/>
+  <!-- <Test drawCanvasFn={drawCanvasFn}/> -->
 </div>
-<br />
-{#if showRandomPhoto}
-  <img id="random-photo" src={randomImageUrl} alt="No dog found" />
-{/if}
-{#if showBreedPhotos}
-  {#each photos as photo}
-    <img id="breed-photos" src={photo} alt="No dog found" />
-  {/each}
-{/if}
-
 <style>
-  #random-photo {
-    width: 600px;
-    height: auto;
-  }
-
-  #breed-photos {
-    width: 300px;
-    height: auto;
-  }
-
-  .btn:focus {
-    border-width: 3px;
+  div {
+    display: flex;
+    background-color: rgba(170, 12, 10, 0.4);
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
   }
 </style>
