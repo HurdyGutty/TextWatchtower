aeab83057cdf3b3b522733ecf42fce098e8fb692
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-08 16:59:12 +0700

    fix: add height and width for canvas to correct canvas coordinates

--- frontend/src/Canvas.svelte
+++ frontend/src/Canvas.svelte
@@ -6,24 +6,29 @@
     let c: CanvasRenderingContext2D;
     let elementRect: DOMRect
     const radius = 5
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    let canvasWidth = 0.92 * vw;
+    let canvasHeight = 0.75 * vh;
+
+    
     onMount(() => {
         c = canvas.getContext('2d')
         elementRect = canvas.getBoundingClientRect()
     })
 
     function clearCanvas() {
-        c.clearRect(0, 0, canvas.width, canvas.height)
+        c.clearRect(0, 0, canvasWidth, canvasHeight)
     }
 
     export function drawCanvas(group: Group) {
         clearCanvas()
-        console.log(elementRect)
         if ("watchBox" in group){
-            c.strokeRect(group.watchBox.x - elementRect.left, group.watchBox.y - elementRect.top, group.watchBox.w, group.watchBox.h)
+            c.strokeRect(group.watchBox.x - elementRect.left - 1, group.watchBox.y - elementRect.top - 1, group.watchBox.w, group.watchBox.h)
         }
         if ("reload" in group) {
             c.beginPath();
-            c.arc(group.reload.X, group.reload.Y, radius, 0, 2 * Math.PI, false);
+            c.arc(group.reload.X - elementRect.left - 1, group.reload.Y - elementRect.top - 1, radius, 0, 2 * Math.PI, false);
             c.fillStyle = "green";
             c.fill();
         }
@@ -32,12 +37,12 @@
 </script>
 
 
-<canvas bind:this={canvas}></canvas>
+<canvas bind:this={canvas} width={canvasWidth} height={canvasHeight}></canvas>
 
 <style>
     canvas {
         border: 1px solid black;
         margin-top: 3.5rem;
-        width: 80vw;
+        padding: 0;
     }
 </style>
