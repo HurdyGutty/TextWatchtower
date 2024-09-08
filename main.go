65bc06184eaa51ceab9d3e298b817640e9cd42b5
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-08 23:59:37 +0700

    refactor: change to GROUP_ID

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"embed"
-	"io/fs"
 	"log"
 	"os"
 
@@ -24,22 +23,6 @@ var assets embed.FS
 //go:embed Tesseract/tessdata/*
 var tesseract embed.FS
 
-func getAllFilenames(efs *embed.FS) (files []string, err error) {
-	if err := fs.WalkDir(efs, ".", func(path string, d fs.DirEntry, err error) error {
-		if d.IsDir() {
-			return nil
-		}
-
-		files = append(files, path)
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return files, nil
-}
-
 func main() {
 	captureGroup.TrainingData = tesseract
 	errorLoad := godotenv.Load()
@@ -47,7 +30,7 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	telegram.Token = os.Getenv("TELEGRAM_BOT_TOKEN")
-	telegram.Chat_ID = os.Getenv("CHAT_ID")
+	telegram.Chat_ID = os.Getenv("GROUP_ID")
 
 	// Create an instance of the app structure
 	app := NewApp()
