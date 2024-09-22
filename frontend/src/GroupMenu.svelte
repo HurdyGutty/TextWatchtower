8fadfed5da95c60642c57d1cecca6c059f042d6f
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-22 13:26:55 +0700

    add: use Input component in GroupMenu

--- frontend/src/GroupMenu.svelte
+++ frontend/src/GroupMenu.svelte
@@ -1,10 +1,52 @@
 <script lang="ts">
-    import { AddNewScreenBox, AssignReloadButton } from "../wailsjs/go/main/App";
+    import { AddNewScreenBox, AssignReloadButton, ChangeMax, ChangeMin } from "../wailsjs/go/main/App";
     import { type Group } from "./stores";
-    import { InstructionInfo } from "../wailsjs/go/instruct/instructionBoard"
+    import { InstructionError, InstructionInfo } from "../wailsjs/go/instruct/instructionBoard"
+    import Input from "./Input.svelte";
 
     export let group: Group;
     export let drawCanvas: (group: Group) => void
+    let min = group.min.toString();
+    let max = group.max.toString();
+
+    function handleMinSubmit(e: Event) {
+        e.preventDefault();
+        let value = Number(min);
+        if (!Number.isInteger(value)) {
+            InstructionError("Minimum time must be an integer")
+            return
+        }
+        if (value < 0) {
+            InstructionError("Maximum time must be larger than 0")
+            return
+        }
+        if (value > group.max){
+            InstructionError("Minimum time must be smaller than maximum time")
+            return
+        }
+        group.min = value;
+        ChangeMin(group.id, group.min)
+        InstructionInfo(`Changed minimum time for ${group.name}`)
+    }
+    function handleMaxSubmit(e: Event) {
+        e.preventDefault();
+        let value = Number(max);
+        if (!Number.isInteger(value)) {
+            InstructionError("Maximum time must be an integer")
+            return
+        }
+        if (value < 0) {
+            InstructionError("Maximum time must be larger than 0")
+            return
+        }
+        if (value < group.min){
+            InstructionError("Maximum time must be larger than minimum time")
+            return
+        }
+        group.max = value;
+        ChangeMax(group.id, group.max)
+        InstructionInfo(`Changed maximum time for ${group.name}`)
+    }
 
 
     async function changeReloadButton(e: Event) {
@@ -44,6 +86,27 @@
         Add new reload button
         {/if}
     </button>
+    <p>
+        <span>Minimum time: </span>
+        <Input
+            id="min"
+            type="number"
+            bind:value={min}
+            on:submit={handleMinSubmit}
+    />
+        <span>s</span>
+    </p>
+
+    <p>
+        <span>Maximum time: </span>
+        <Input
+        id="max"
+        type="number"
+        bind:value={max}
+        on:submit={handleMaxSubmit}
+    />
+    <span>s</span>
+    </p>
 </div>
 
 <style>
