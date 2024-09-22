b9368d25bbec112a265da27e0cd59b2e1a47619c
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-09-22 13:28:20 +0700

    add: new GroupName component  with animation for long name

---
+++ frontend/src/GroupName.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { type Group } from "./stores";
+
+    export let toggleRename: svelte.JSX.EventHandler<Event>
+    export let group: Group
+
+</script>
+
+<p>
+    <span on:click={toggleRename}
+    on:keyup={toggleRename}
+    role="button"
+    tabindex="0">
+    {group.name}
+    </span>
+</p>
+
+<style>
+    p {
+        flex-grow: 0.6;
+        overflow-x: hidden;
+        white-space: nowrap;
+        box-sizing: border-box;
+        position: relative;
+        max-width: 4rem;
+    }
+    span{
+        display: inline-block;
+        position: relative;
+        font-weight: bold;
+    }
+    span:hover{
+        animation: move 5s ease-out infinite;
+    }
+    @keyframes move {
+        25% { transform: translate(0, 0); left: 0%; }
+        75%, 100% { transform: translate(-100%, 0); left: 100%; }
+    }
+</style>
