f4056ec2f86b20d6b57232c8119f16452dcda15a
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-10-02 16:51:55 +0700

    add: appearance for input number

--- frontend/src/Input.svelte
+++ frontend/src/Input.svelte
@@ -34,16 +34,15 @@
 />
 
 <style>
-    /* Chrome, Safari, Edge, Opera */
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
     }
 
-    /* Firefox */
     input[type=number] {
     -moz-appearance: textfield;
+    appearance: textfield;
     }
 
     #name{
